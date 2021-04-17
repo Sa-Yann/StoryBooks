3650532175d43b26e0d1f431c8d35e8ba9e0a196
Yann Leblond <knt4xyz@gmail.com>
2021-04-17 21:19:28 +0200

    📝 Made sure the Login page got his own structure
    adding an objectto app.get du login  stipulating which file to refer for layout architecture
    change the login.hrs name to login_layout to make it more readable

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,10 @@ const router = express.Router()
 // Login / Landing page render via a .get(){} to /
 router.get('/', (req, res) => {
     // res.send('logiiiiiiiin') to test before creating the handelbars loginview
-    res.render('login_view')
+    res.render('login_view', {
+        // here we specify that we want the login view to render the login_view based on the login_layout.hrs file in the layouts folder
+        layout: 'login_layout'
+    })
 })
 
 // Dashboard page render via a .get(){} to /dashboard
